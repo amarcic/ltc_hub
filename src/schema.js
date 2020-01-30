5ba26a81f6e3ae312ee59767a5c05b10f951670d
amarcic <aleksander.marcic@gmail.com>
2020-01-30 18:43:15 +0100

    set up the project; initiate Apollo Server; first Draft for Schema

---
+++ src/schema.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Entity {
+        identifier: ID!
+        name(language: Languages): String
+        description: String
+        isA: Subject
+        isPartOf: Entity
+        spacial: Place
+        temporal: Period
+    }
+    type Subject {
+        URID: ID!
+        title: String
+    }
+    type Place {
+        URID: ID!
+        titles(language: Languages): String
+        location: String
+    }
+    type Title{
+        language: Languages
+        label: String 
+    }
+    type Period {
+        URID: ID!
+        title(language: Languages): String
+        begin: String
+        end: String
+    }
+    enum Languages {
+        de
+        en
+        it
+        fr
+        ar
+    }
+    type Query {
+        entities: [Entity]
+        entitiy(identifier: ID!): Entity 
+    }
+`;
+
+module.exports = typeDefs;
