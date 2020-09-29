2b736c666f4f0f8992fc200957c10f3f9ac49731
amarcic <aleksander.marcic@gmail.com>
2020-09-29 18:04:59 +0200

    dateParserArachne function husk set up

--- src/serviceFunctions.js
+++ src/serviceFunctions.js
@@ -1,7 +1,14 @@
-const dateParserArachne = (dateingArray) => {
-    let dates = dateingArray//.forEach( strng => strng + "!");
+const dateParserArachne = (datingArray) => {
+    const dates = datingArray;
+    let dateObj = {
+        begin: {start: "", end: ""},
+        end: {start: "", end: ""}
+    };
+    //put magic here
+    let newDates = dates.map( date => date);
 
-    return dates;
+    //later dateObj will be returned
+    return newDates;
 }
 
 exports.dateParserArachne = dateParserArachne;
